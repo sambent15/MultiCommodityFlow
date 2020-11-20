9f0c8baa15c14e938efaf54c66d90e835c60aefe
Samy Bentayeb <samy.bentayeb@hotmail.fr>
2020-11-20 15:43:05 +0100

    Cout minimum

--- main.py
+++ main.py
@@ -2,7 +2,6 @@ from __future__ import print_function
 from ortools.graph import pywrapgraph
 
 
-
 def main():
     arc_depart = [0, 0, 1, 1, 1, 2, 2, 3, 4]
     arc_arrivee = [1, 2, 2, 3, 4, 3, 4, 4, 2]
@@ -11,4 +10,31 @@ def main():
 
     offre = [20, 0, 0, -5, -15]
 
+    min_cost_flow = pywrapgraph.SimpleMinCostFlow()
 
+    for i in range(0, len(arc_depart)):
+        min_cost_flow.AddArcWithCapacityAndUnitCost(arc_depart[i], arc_arrivee[i],
+                                                    capa[i], cout[i])
+
+    for i in range(0, len(offre)):
+        min_cost_flow.SetNodeSupply(i, offre[i])
+
+        # Find the minimum cost flow between node 0 and node 4.
+    if min_cost_flow.Solve() == min_cost_flow.OPTIMAL:
+        print('Le cout minimum est :', min_cost_flow.OptimalCost())
+        print('')
+        print('  Arc    Flot / Capacité  Cout')
+        for i in range(min_cost_flow.NumArcs()):
+            cost = min_cost_flow.Flow(i) * min_cost_flow.UnitCost(i)
+            print('%1s -> %1s   %3s  / %3s       %3s' % (
+                min_cost_flow.Tail(i),
+                min_cost_flow.Head(i),
+                min_cost_flow.Flow(i),
+                min_cost_flow.Capacity(i),
+                cost))
+    else:
+        print('Probleme avec le input.')
+
+
+if __name__ == '__main__':
+    main()
